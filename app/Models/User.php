c04e9441f9613a2cf5495c9c9d0a1132df7f7691
Mykola Bokoch <bokoch1995@gmail.com>
2021-04-04 22:21:21 +0300

    Added posts relation to user and fixed faker data in category factory.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Post\Post;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use RushApp\Core\Models\User as BaseUser;
 
 class User extends BaseUser {
 
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
